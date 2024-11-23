599275307ecd2c25e4fc20877db53b861d0cf45d
Chen Lay <maungmaungyoker.mmy@gmail.com>
2024-11-23 16:34:31 +0700

    feat: add domain enumerations

---
+++ src/core/domain/enums/notification-type.enum.ts
@@ -0,0 +1,11 @@
+export enum NotificationType {
+  SUBSCRIPTION_EXPIRING = 'subscription_expiring',
+  SUBSCRIPTION_EXPIRED = 'subscription_expired',
+  PAYMENT_SUCCESS = 'payment_success',
+  PAYMENT_FAILED = 'payment_failed',
+  REQUEST_LIMIT_EXCEEDED = 'request_limit_exceeded',
+  LOGIN_ATTEMPT = 'login_attempt',
+  ROLE_UPDATED = 'role_updated',
+  LIMIT_UPDATED = 'limit_updated',
+  PAYMENT_APPROVED = 'payment_approved',
+}
